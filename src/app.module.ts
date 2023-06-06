3cb71a4384c244576b863a4221f69f3271c7553f
Azamat Jumabekov <so.fly.azash@gmail.com>
2023-06-06 15:58:27 +0600

    users

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+
+
+@Module({
+    controllers: [],
+    providers: [],
+    imports: [
+        SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'jumabekovs',
+          password: 'root',
+          database: 'nest-course',
+          models: [],
+          autoLoadModels: true
+        }),
+        UsersModule,
+      ],
+})
+export class AppModule {}
